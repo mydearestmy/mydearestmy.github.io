8a279b34450636fb4dd8f755c3f920fd6e26533b
mydearestmy <s4035889@rmit.edu.vn>
2025-06-04 02:13:38 +1000

    practice

---
+++ week10/dragdrop.js
@@ -0,0 +1,23 @@
+const card = document.querySelector(".card");
+console.log(card);
+let draggedCard = null;
+
+card.addEventListener("dragstart", function () {
+  draggedCard = card;
+  console.log(draggedCard);
+});
+const dropBox = document.querySelector(".dropbox");
+dropBox.innerHTML = "";
+
+dropBox.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+dropBox.addEventListener("drop", function () {
+  const clone = draggedCard;
+  clone.classList.remove("flip");
+  clone.addEventListener("click", function () {
+    clone.classList.toggle("flip");
+  });
+  dropBox.appendChild(clone);
+});
